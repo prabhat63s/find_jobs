2925f9995869c148a12215e651393c08b4dbc71a
prabhat63s <prabhat6914@gmail.com>
2024-08-14 12:50:39 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,48 @@
+import express, { urlencoded } from "express";
+import connectDB from "./db/connection.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import bodyParser from "body-parser";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+
+import path from "path";
+
+dotenv.config();
+// connect db
+connectDB();
+const PORT = process.env.PORT || 8080;
+
+const __dirname = path.resolve();
+
+const app = express();
+
+
+// middleware
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin:"http://localhost:5173",
+    credentials:true
+}
+app.use(cors(corsOptions));
+
+// api's route
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`server running at port ${PORT}`);
+});
