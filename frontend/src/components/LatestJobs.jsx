7d84fb398aebd67c5dc43f67021c488e4ab4d464
prabhat63s <prabhat6914@gmail.com>
2024-09-07 15:57:05 +0530

    issue fixed

--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -1,42 +1,10 @@
-// import React, { useState } from "react";
-// import LatestJobCard from "./LatestJobCard";
-// import { useSelector } from "react-redux";
-// import { Link } from "react-router-dom";
-
-// const LatestJobs = () => {
-//   const { allJobs } = useSelector((store) => store.job);
-//   return (
-//     <div className="max-w-7xl mx-auto my-20">
-//       <h1 className="text-4xl font-bold">
-//         <span className="text-[#6A38C2]">Latest</span> Job Openings
-//       </h1>
-//       <div className="grid grid-cols-3 gap-4 my-5">
-//         {allJobs &&
-//           allJobs?.slice(0, 6).map((job) => (
-//             <Link key={job._id} to={`/description/${job?._id}`}>
-//               <LatestJobCard job={job} />
-//             </Link>
-//           ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LatestJobs;
-import React from "react";
-import { useSelector } from "react-redux";
-import { Link, Redirect } from "react-router-dom";
+import React, { useState } from "react";
 import LatestJobCard from "./LatestJobCard";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const LatestJobs = () => {
   const { allJobs } = useSelector((store) => store.job);
-  const { authUser } = useSelector((store) => store.auth);
-
-  if (!authUser) {
-    // Redirect to login page if the user is not authenticated
-    return <Redirect to="/login" />;
-  }
-
   return (
     <div className="max-w-7xl mx-auto my-20">
       <h1 className="text-4xl font-bold">
@@ -44,8 +12,8 @@ const LatestJobs = () => {
       </h1>
       <div className="grid grid-cols-3 gap-4 my-5">
         {allJobs &&
-          allJobs.slice(0, 6).map((job) => (
-            <Link key={job._id} to={`/description/${job._id}`}>
+          allJobs?.slice(0, 6).map((job) => (
+            <Link key={job._id} to={`/description/${job?._id}`}>
               <LatestJobCard job={job} />
             </Link>
           ))}
